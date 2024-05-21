cd68690584c82efc3bf72ce9522aa44a5a377724
dnephew1 <100165961+dnephew1@users.noreply.github.com>
2024-05-21 01:23:14 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,5 +21,7 @@ npm install --no-audit
 # Update global npm packages without auditing
 npm update -g --no-audit
 
+sudo apt-get install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libgbm-dev
+
 # Provide feedback to the user
 echo "Setup complete. index.js and .env files created if they didn't exist, npm packages installed, global npm packages updated, and .env file populated with the OpenAI API key."
